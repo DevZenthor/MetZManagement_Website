608044bb4daa7fc12808c0477017ba8df63e3c2a
DevZenthor <zenthorpro@outlook.com>
2025-09-29 19:24:52 +0200

    fix: modify the css of navbar and homepage

--- MetZManagement_Website/src/components/Navbar/Navbar.jsx
+++ MetZManagement_Website/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png"; 
-import "./Navbarr.css";
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
